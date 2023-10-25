ce68dbd382c0aed7b5a4b918939e1dcac981c2be
Nele Custers <custersnele@gmail.com>
2023-10-25 13:32:08 +0200

    initial files

---
+++ src/main/java/be/pxl/dice/RollTheDieApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollTheDieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollTheDieApplication.class, args);
+	}
+
+}
